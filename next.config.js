39f69b0d0fb99bf9da84eb506ce00149b032296a
Xinyue Wang <102530413+xinyue0221@users.noreply.github.com>
2025-03-28 22:00:55 -0700

    update

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // Required for static export
   },
-  ...(process.env.NODE_ENV === 'production' ? {
-    basePath: '/lu-ocp-website',
-    assetPrefix: '/lu-ocp-website/',
-  } : {}),
 }
 
 module.exports = nextConfig 
